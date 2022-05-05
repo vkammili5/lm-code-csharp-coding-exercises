2f2478f962c5d76fbc0f3ccbb7cfb109f6160437
vkammili5 <vijaylakshmikammili@gmail.com>
2022-05-05 22:51:58 +0100

    Exercise003 completed

--- Exercises/Exercise003.cs
+++ Exercises/Exercise003.cs
@@ -4,9 +4,21 @@ namespace Exercises
     public class Exercise003
     {
         // Replace the exception statement below with your code!
-        public string[] IceCreamFlavours => throw new NotImplementedException();
+        public string[] IceCreamFlavours = { "Pistachio", "Raspberry Ripple", "Vanilla", "Mint Chocolate Chip", "Chocolate", "Mango Sorbet" };
 
         // Replace the exception statement below with your code!
-        public int IceCreamCode(string iceCreamFlavour) => throw new NotImplementedException();
+        public int IceCreamCode(string iceCreamFlavour)
+        {
+            for(int i = 0; i < IceCreamFlavours.Length; i++)
+            {
+                if(IceCreamFlavours[i] == iceCreamFlavour)
+                {
+                    return i;
+                }
+            }
+            return -1;
+            
+        }
+
     }
 }
