7f8a26eae7c8b9bdd1e71ead31fa28bb7c939cbd
vkammili5 <vijaylakshmikammili@gmail.com>
2022-05-05 22:32:12 +0100

    Exercise002 completed

--- Exercises/Exercise002.cs
+++ Exercises/Exercise002.cs
@@ -1,18 +1,31 @@
 ﻿using System;
+using Exercises.Models;
+
 namespace Exercises
 {
     public class Exercise002
     {
-        public bool IsFromManchester(/* Person person */)
+        public bool IsFromManchester(Person person)
         {
             // Replace the exception statement below with your code!
-            throw new NotImplementedException();
+            //throw new NotImplementedException();
+            if (person.City == "Manchester")
+            {
+                return true;
+
+            }
+            return false;
         }
 
-        public bool CanWatchFilm(/*Person person, int ageLimit*/)
+        public bool CanWatchFilm(Person person, int ageLimit)
         {
             // Replace the exception statement below with your code!
-            throw new NotImplementedException();
+            //throw new NotImplementedException();
+            if(person.Age  < ageLimit)
+            {
+                return false;
+            }
+            return true;
         }
     }
 }
---
+++ Exercises/Models/Person.cs
@@ -0,0 +1,23 @@
+﻿using System;
+namespace Exercises.Models
+{
+    public class Person
+    {
+
+        public Person(string firstname, string lastname, string city, int age)
+        {
+            FirstName = firstname;
+            LastName = lastname;
+            City = city;
+            Age = age;
+        }
+
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public string City { get; set; }
+
+        public int Age { get; set; }
+    }
+}
