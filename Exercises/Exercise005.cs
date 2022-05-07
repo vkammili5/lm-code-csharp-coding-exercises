70a0f9ac82b59fe49669ae7cc4b6236657f48445
vkammili5 <vijaylakshmikammili@gmail.com>
2022-05-07 12:25:15 +0100

    Exercise005 completed

--- Exercises/Exercise005.cs
+++ Exercises/Exercise005.cs
@@ -1,4 +1,8 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+
 namespace Exercises
 {
     public class Exercise005
@@ -6,7 +10,16 @@ namespace Exercises
         public bool IsPangram(string input)
         {
             // Replace the exception statement below with your code!
-            throw new NotImplementedException();
+            //throw new NotImplementedException();
+            if (string.IsNullOrEmpty(input))
+            {
+                return false;
+            }
+            else 
+            {
+                return input.ToLower().Where(ch => Char.IsLetter(ch)).GroupBy(ch => ch).Count() == 26;
+            }
+            
         }
     }
 }
